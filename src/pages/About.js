c00df3962d89d0b71fbb30049148860dd63496d6
Akash <aku.das02@gmail.com>
2021-01-20 23:37:30 +0600

    about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,33 @@ import React from "react";
 
 const About = () => {
   return (
-    <>
-      <h1>About Page</h1>
-    </>
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        We are a small family-owned business operating one of the world's
+        largest and most visited drinks related websites. The Cocktail DB is
+        aimed at providing you and other discerning drinkers with factual and
+        impartial information on what to drink and where to drink it, along with
+        our opinion on what's good, bad or just indifferent.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore,
+        corporis? Quidem dolores, explicabo rem libero labore aliquid! Aliquid
+        quam corrupti saepe iusto magni! Animi quo necessitatibus pariatur.
+        Eius, officia neque!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
+        exercitationem laborum? Laudantium quia quibusdam iusto minima, modi
+        dolor fuga nihil, architecto vero perferendis dolorem nisi vitae quidem.
+        Dolores nobis perspiciatis dignissimos itaque at, eos, mollitia quasi
+        quidem earum tenetur sed tempore porro voluptatum, consequatur atque
+        nostrum sapiente similique vero assumenda aliquid deserunt debitis
+        repudiandae ut iste? Ipsum voluptate qui explicabo? Enim velit maiores
+        voluptates vel, ducimus cum minima vero dicta excepturi aperiam
+        distinctio sed et nemo ad rem ullam officia.
+      </p>
+    </section>
   );
 };
 
