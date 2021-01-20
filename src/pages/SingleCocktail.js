be237b95bba2db14ef9f620f4584499d43d60796
Akash <aku.das02@gmail.com>
2021-01-20 22:43:55 +0600

    single cocktail setup - url parameters

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -1,9 +1,11 @@
 import React from "react";
+import {useParams} from 'react-router-dom';
 
 const SingleCocktail = () => {
+  const {id} = useParams();
   return (
     <>
-      <h1>SingleCocktail Page</h1>
+      <h1>SingleCocktail Page | id: {id}</h1>
     </>
   );
 };
