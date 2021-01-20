42df32ea2eacb55942c034227cb0fd5a2d051926
Akash <aku.das02@gmail.com>
2021-01-20 23:58:18 +0600

    home page setup

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import SearchForm from "../components/SearchForm";
+import CocktailList from "../components/CocktailList";
 
 const Home = () => {
+  const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("a");
+  const [cocktails, setCocktails] = useState([]);
+
   return (
-    <>
-      <h1>Home Page</h1>
-    </>
+    <main>
+      <SearchForm setSearchTerm={setSearchTerm} />
+      <CocktailList loading={loading} cocktails={cocktails} />
+    </main>
   );
 };
 
