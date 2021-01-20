df3db44604505d7ec51e1df1894a36e8422be8a6
Akash <aku.das02@gmail.com>
2021-01-20 22:03:32 +0600

    folder structure and basic setup

---
+++ src/components/CocktailList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CocktailList = () => {
+  return (
+    <>
+      <h1>CocktailList Component</h1>
+    </>
+  );
+};
+
+export default CocktailList;
---
+++ src/pages/SingleCocktail.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SingleCocktail = () => {
+  return (
+    <>
+      <h1>SingleCocktail Page</h1>
+    </>
+  );
+};
+
+export default SingleCocktail;
